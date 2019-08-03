f97cb4be4504fcb304a3d3694ee8cff88e70b82a
Brian Noble <brian@sunnybyte.com>
2019-08-03 09:52:54 -0700

    functions: add new twig function and filters, update composer.jsonn and readme

--- README.md
+++ README.md
@@ -22,17 +22,114 @@ To install the plugin, follow these instructions.
 
 3. In the Control Panel, go to Settings → Plugins and click the “Install” button for Twig Extender.
 
-## Twig Extender Overview
+## Filters
 
--Insert text here-
+###vartype
 
-## Configuring Twig Extender
+Returns the variable type
+   
+```php
+{% set variable = "Whammy!" %}
 
--Insert text here-
+{{ variable|vartype }}
 
-## Using Twig Extender
+#Output
 
--Insert text here-
+string
+```
+###striphttp
+
+Removes the protocol from a URL.
+
+```php 
+{{ "https://sunnybyte.com"|striphttp }}
+
+#Output
+
+"sunnybyte.com"
+```
+
+###phone
+
+Formats US phone numbers
+
+```php
+{{ "15555555555"|phone }}
+{{ "15555555555"|phone('.') }}
+{{ "15555555555"|phone('-', 'parens') }}
+
+#Output
+'1-555-555-5555'
+'1.555.555.5555'
+'1 (555) 555-5555'
+```
+_Arguments_  
+$separators `string` = What to separate the phone number with.  
+$parens `string` = Parenthesize the area code, **'parens'** is the only value allowed
+
+
+###truncate
+
+Truncates a string to specified a character limit and appends an ending to the string
+
+```php
+{% set paragraph = "Hello, this is my paragraph, use me to test some of our twig functions." %}
+
+{{ paragraph|truncate(27) }}
+{{ paragraph|truncate(5, ' World') }}
+
+#Output
+"Hello, this is my paragraph..."
+"Hello World" 
+```
+
+_Arguments_  
+$limit `integer` = The characters the string will be limited to.
+
+$end `string` = What will be appended to the string. _default_ ...
+
+##Functions
+
+###isVarType
+
+Returns true if the argument matches the variable type
+```php
+{% set variable = "Whammy!" %}
+
+{% if isVarType(variable, 'string' %}
+    {{ variable }}
+{% endif %}
+
+{% if isVarType(variable, 'integer' %}
+    {{ variable }}
+{% else %}
+    I'm not an integer!
+{% if %}
+
+#Output
+'Whammy!'
+'I'm not an integer!'
+```
+
+_Arguments_  
+$type `string` = what type to check the variable against.
+
+_Variable Types_
+- array
+- boolean
+- double
+- float
+- integer
+- NULL
+- object
+- resource
+- string 
+
+###relativeTime 
+###formatVideoUrl
+###truncate
+###oembed
+###ordinal
 
 ## Twig Extender Roadmap
 
--- src/twigextensions/TwigExtenderTwigExtension.php
+++ src/twigextensions/TwigExtenderTwigExtension.php
@@ -50,7 +50,10 @@ class TwigExtenderTwigExtension extends \Twig_Extension
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('someFilter', [$this, 'someInternalFunction']),
+            new \Twig_SimpleFilter('vartype', [$this, 'getVarType']),
+            new \Twig_SimpleFilter('truncate', [$this, 'truncate']),
+            new \Twig_SimpleFilter('striphttp', [$this, 'stripHttp']),
+            new \Twig_SimpleFilter('phone', [$this, 'formatPhone'])
         ];
     }
 
@@ -64,21 +67,282 @@ class TwigExtenderTwigExtension extends \Twig_Extension
     public function getFunctions()
     {
         return [
-            new \Twig_SimpleFunction('someFunction', [$this, 'someInternalFunction']),
+            new \Twig_SimpleFunction('isVarType', [$this, 'checkVariableType']),
+            new \Twig_SimpleFunction('relativeTime', [$this, 'relativeTime']),
+            new \Twig_SimpleFunction('truncate', [$this, 'truncate']),
+            new \Twig_SimpleFunction('formatVideoUrl', [$this, 'formatVideoUrl']),
+            new \Twig_SimpleFunction('oembed', [$this, 'oEmbed']),
+            new \Twig_SimpleFunction('ordinal', [$this, 'ordinal'])
         ];
     }
 
+
     /**
-     * Our function called via Twig; it can do anything you want
+     * Checks if variable type matches parameter $type.
+     * @param $var
+     * @param null $type
+     * @return bool
+     */
+    public function checkVariableType($var, $type=null)
+    {
+        switch ($type)
+        {
+            case 'array':
+                return is_array($var);
+                break;
+
+            case 'boolean':
+                return is_bool($var);
+                break;
+
+            case 'double':
+                return is_double($var);
+                break;
+
+            case 'float':
+                return is_float($var);
+                break;
+
+            case 'integer':
+                return is_int($var);
+                break;
+
+            case 'NULL':
+                return is_null($var);
+                break;
+
+            case 'numeric':
+                return is_numeric($var);
+                break;
+
+            case 'object':
+                return is_object($var);
+                break;
+
+            case 'resource':
+                return is_resource($var);
+                break;
+
+            case 'string':
+                return is_string($var);
+                break;
+        }
+    }
+
+    /**
+     * Outputs the variable type
      *
-     * @param null $text
-     *
+     * @param $var
      * @return string
      */
-    public function someInternalFunction($text = null)
-    {
-        $result = $text . " in the way";
 
-        return $result;
+    public function getVarType($var) {
+        return gettype($var);
     }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    public function formatVideoUrl($string)
+    {
+        $parsed = parse_url($string);
+
+        switch ($parsed['host'])
+        {
+            case 'vimeo.com':
+                $url = 'https://player.vimeo.com/video' . $parsed['path'];
+            break;
+
+            case 'youtube.com':
+            default:
+
+                if (stripos($string, 'youtube.com/embed/') !== false)
+                {
+                    // good to go!
+                    return $string;
+                }
+
+                $url = 'https://www.youtube.com/embed'. $parsed['path'];
+
+            break;
+        }
+
+        return $url;
+    }
+
+    /**
+     * Converts a string of numbers to a US phone number formate
+     *
+     * @param $string
+     * @param string $separator
+     * @param null $parens
+     * @return string|string[]|null
+     */
+    public function formatPhone($string, $separator="-", $parens=null)
+    {
+        if (empty($string))
+        {
+            return '';
+        }
+
+        $phoneNumber = preg_replace('/[^0-9]/', '', $string);
+
+        if ($parens === 'parens')
+        {
+            if (strlen($phoneNumber) > 10)
+            {
+                $countryCode = substr($phoneNumber, 0, strlen($phoneNumber)-10);
+                $areaCode = substr($phoneNumber, -10, 3);
+                $nextThree = substr($phoneNumber, -7, 3);
+                $lastFour = substr($phoneNumber, -4, 4);
+
+                $phoneNumber = $countryCode . ' ' . '(' . $areaCode . ')' . ' ' . $nextThree . $separator . $lastFour;
+            }
+
+            else if(strlen($phoneNumber) == 10)
+            {
+                $areaCode = substr($phoneNumber, 0, 3);
+                $nextThree = substr($phoneNumber, 3, 3);
+                $lastFour = substr($phoneNumber, 6, 4);
+
+                $phoneNumber = '(' . $areaCode . ')' . ' ' . $nextThree . $separator . $lastFour;
+            }
+            else if(strlen($phoneNumber) == 7)
+            {
+                $nextThree = substr($phoneNumber, 0, 3);
+                $lastFour = substr($phoneNumber, 3, 4);
+
+                $phoneNumber = $nextThree . $separator . $lastFour;
+            }
+        }
+        else
+        {
+            if (strlen($phoneNumber) > 10)
+            {
+                $countryCode = substr($phoneNumber, 0, strlen($phoneNumber)-10);
+                $areaCode = substr($phoneNumber, -10, 3);
+                $nextThree = substr($phoneNumber, -7, 3);
+                $lastFour = substr($phoneNumber, -4, 4);
+
+                $phoneNumber = $countryCode . $separator . $areaCode . $separator . $nextThree . $separator . $lastFour;
+            }
+            else if(strlen($phoneNumber) == 10)
+            {
+                $areaCode = substr($phoneNumber, 0, 3);
+                $nextThree = substr($phoneNumber, 3, 3);
+                $lastFour = substr($phoneNumber, 6, 4);
+
+                $phoneNumber = $areaCode . $separator . $nextThree . $separator . $lastFour;
+            }
+            else if(strlen($phoneNumber) == 7)
+            {
+                $nextThree = substr($phoneNumber, 0, 3);
+                $lastFour = substr($phoneNumber, 3, 4);
+
+                $phoneNumber = $nextThree . $separator . $lastFour;
+            }
+        }
+
+        return $phoneNumber;
+    }
+
+    /**
+     * Removes the protocol from a URL
+     *
+     * @param $string
+     * @return string
+     */
+    public function stripHttp($string)
+    {
+        $parsed = parse_url($string);
+
+        if (empty($parsed['host']))
+        {
+            return $string;
+        }
+
+        if (empty($parsed['path']))
+        {
+            return $parsed['host'];
+        }
+
+        return $parsed['host'] . ($parsed['path'] === '/' ? '' : $parsed['path']);
+    }
+
+    /**
+     * Shortens a string to the specified limit and append to elepses end of the string
+     *
+     * @param $value
+     * @param $limit
+     * @param string $end
+     * @return string
+     */
+    public function truncate($value, $limit, $end = '...')
+    {
+        if (mb_strwidth($value, 'UTF-8') <= $limit) {
+            return $value;
+        }
+
+        return rtrim(mb_strimwidth($value, 0, $limit, '', 'UTF-8')).$end;
+    }
+
+    /**
+     * @param \DateTime $dateTime
+     * @return string|null
+     * @throws \Exception
+     */
+    public function relativeTime(\DateTime $dateTime)
+    {
+        $now = new \DateTime();
+        $interval = $now->diff($dateTime);
+
+        $days = (int)$interval->format('%d');
+        $hours = (int)$interval->format('%h');
+        $minutes = (int)$interval->format('%i');
+        $seconds = (int)$interval->format('%s');
+
+        $suffix = ! empty($suffix) ? ' ' . $suffix : '';
+
+        if ($days > 0)
+        {
+            return $days . 'd' . $suffix;
+        }
+
+        if ($hours > 0)
+        {
+            return $hours . 'hr' . $suffix;
+        }
+
+        if ($minutes > 0)
+        {
+            return $minutes . 'm' . $suffix;
+        }
+
+        if ($seconds > 0)
+        {
+            return $seconds . 's' . $suffix;
+        }
+
+        return NULL;
+
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    public function ordinal($string)
+    {
+        $fmt = new \NumberFormatter( 'en_US', \NumberFormatter::ORDINAL );
+
+        return $fmt->format($string);
+    }
+}
+
+class Oembed
+{
+    public $thumb;
+
+    public $iframeUrl;
 }
